9830b30aa37c647c476f0ca31c746b9da77f3d5f
Denys Samoilenko <denniksam@gmail.com>
2023-01-31 15:49:03 +0200

    Calc--1

--- app/src/main/java/step/learning/basics/MainActivity.java
+++ app/src/main/java/step/learning/basics/MainActivity.java
@@ -2,6 +2,7 @@ package step.learning.basics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,15 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addButton = findViewById( R.id.addButton ) ;
-        addButton.setOnClickListener( this::addButtonClick ) ;
+        Button addButton = findViewById( R.id.exitButton ) ;
+        addButton.setOnClickListener( this::exitButtonClick ) ;
+
+        findViewById( R.id.calcButton )
+                .setOnClickListener( this::calcButtonClick ) ;
     }
 
-    private void addButtonClick( View v ) {
-        TextView tvHello = findViewById( R.id.tvHello ) ;
-        String txt = tvHello.getText().toString() ;
-        txt += "!" ;
-        tvHello.setText( txt ) ;
+    private void calcButtonClick( View v ) {
+        Intent calcIntent = new Intent( this, CalcActivity.class ) ;
+        startActivity( calcIntent ) ;
+    }
+    private void exitButtonClick( View v ) {
+        finish() ;
     }
 }
 /*
