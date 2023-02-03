7f4e41208eb6859ae318c3159a2b30cf0c654da7
Denys Samoilenko <denniksam@gmail.com>
2023-02-03 11:43:38 +0200

    Styles

--- app/src/main/java/step/learning/basics/CalcActivity.java
+++ app/src/main/java/step/learning/basics/CalcActivity.java
@@ -3,12 +3,48 @@ package step.learning.basics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CalcActivity extends AppCompatActivity {
+    private TextView tvHistory ;
+    private TextView tvResult ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        tvHistory = findViewById( R.id.tvHistory ) ;
+        tvResult  = findViewById( R.id.tvResult  ) ;
+        tvHistory.setText( "" ) ;
+        tvResult.setText( "0" ) ;
+
+        findViewById( R.id.btn7 ).setOnClickListener( this::digitClick ) ;
+        findViewById( R.id.btnPlusMinus ).setOnClickListener( this::pmClick ) ;
+    }
+    private void pmClick( View v ) {  // изменение знака (плюс/минус)
+
+    }
+    private void digitClick( View v ) {
+        // задача: ограничить величиной в 10 цифр
+        String result = tvResult.getText().toString() ;
+        if( result.length() >= 10 ) return ;
+
+        String digit = ((Button) v).getText().toString() ;
+        if( result.equals( "0" ) ) {
+            result = digit ;
+        }
+        else {
+            result += digit ;
+        }
+        tvResult.setText( result ) ;
     }
 }
+/*
+Д.З. Реализовать функции калькулятора:
+- набор цифр (не более 10 цифр на экране)
+- изменение знака (плюс/минус) - знак не считается за цифру, ограничение - 10 именно цифр
+- десятичная точка - тоже не считается за цифру + ограничить набор более одной точки
+ */
