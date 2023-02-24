34af9a52eda85d378f2cf8c07036099604b01740
Denys Samoilenko <denniksam@gmail.com>
2023-02-24 14:54:28 +0200

    Notifications

--- app/src/main/java/step/learning/basics/ChatActivity.java
+++ app/src/main/java/step/learning/basics/ChatActivity.java
@@ -1,12 +1,26 @@
 package step.learning.basics;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -24,31 +38,82 @@ import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
 public class ChatActivity extends AppCompatActivity {
-    private final String CHAT_URL = "https://diorama-chat.ew.r.appspot.com/story" ;
-    private String content ;
-    private LinearLayout chatContainer ;
-    private List<ChatMessage> chatMessages ;
-    private ChatMessage chatMessage ;
-    private EditText etAuthor ;
-    private EditText etMessage ;
+    private final String CHAT_URL = "https://diorama-chat.ew.r.appspot.com/story";
+    private String content;
+    private LinearLayout chatContainer;
+    private List<ChatMessage> chatMessages;
+    private ChatMessage chatMessage;
+    private EditText etAuthor;
+    private EditText etMessage;
+    private ScrollView svContainer;
+    private Handler handler;   // управление отложенным запуском (в т.ч. таймером)
+    private static final String CHANNEL_ID = "chat_channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        new Thread( this::loadUrl ).start() ;
+        // new Thread(this::loadUrl).start();
 
-        chatContainer = findViewById( R.id.chatContainer ) ;
-        etAuthor = findViewById( R.id.etUserName ) ;
-        etMessage = findViewById( R.id.etMessage ) ; 
-        findViewById( R.id.chatButtonSend ).setOnClickListener( this::sendButtonClick ) ;
+        chatContainer = findViewById(R.id.chatContainer);
+        svContainer = findViewById(R.id.sv_container);
+        etAuthor = findViewById(R.id.etUserName);
+        etMessage = findViewById(R.id.etMessage);
+        findViewById(R.id.chatButtonSend).setOnClickListener(this::sendButtonClick);
+
+        chatMessages = new ArrayList<>();
+        handler = new Handler();
+        handler.post( this::updateChat ) ;
+        // handler.postDelayed(this::showNotification, 2000);
+    }
+    private void updateChat() {   // периодически запускаемый метод проверки сообщений
+        new Thread( this::loadUrl ).start() ;
+        handler.postDelayed( this::updateChat, 2000 ) ;
+    }
+    /*
+    Разделить регистрацию канала и отправку сообщений
+    При появлении новых сообщений (showChatMessages - needScrollDown) формировать уведомление
+     */
+    private void showNotification() {
+        // https://developer.android.com/develop/ui/views/notifications/channels#java
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle("Chat")
+                        .setContentText("Message from chat")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Notification notification = builder.build();
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(ChatActivity.this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(ChatActivity.this,
+                        new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 100);
+                return;
+            }
+        }
+        notificationManager.notify(1001, notification);
     }
     private void sendButtonClick( View view ) {
         this.chatMessage = new ChatMessage() ;
@@ -125,25 +190,24 @@ public class ChatActivity extends AppCompatActivity {
             Log.d( "loadUrl", "IOException: " + ex.getMessage() ) ;
         }
     }
-    private void showChatMessages() {
-        TextView tv = new TextView( this ) ;
-        String msg = "" ;
-        for( ChatMessage chatMessage : this.chatMessages ) {
-            msg += chatMessage.getMoment() + ": " +chatMessage.getTxt() + "\n" ;
-        }
-        tv.setText( msg ) ;
-        chatContainer.addView( tv ) ;
-    }
     private void parseContent() {
         try {                                                       // server response:
             JSONObject js = new JSONObject( content ) ;             // { "status": "success",
             JSONArray jMessages = js.getJSONArray( "data" ) ;     //   "data": [ {},{},... ]
             if( "success".equals( js.get("status") ) ) {            // }
-                chatMessages = new ArrayList<>() ;  // TODO: см. ТЗ
+                // chatMessages = new ArrayList<>() ;  // TODO: см. ТЗ - перенесено в onCreate
                 for( int i = 0; i < jMessages.length(); ++i ) {
-                    chatMessages.add(
-                            new ChatMessage( jMessages.getJSONObject( i ) ) ) ;
+                    ChatMessage tmp = new ChatMessage( jMessages.getJSONObject( i ) ) ;
+                    // проверяем есть ли это сообщение в коллекции
+                    if (chatMessages.stream().noneMatch( obj -> obj.getId().equals( tmp.getId() ) ) ) {
+                        chatMessages.add( tmp ) ;  // если нет, то добавляем
+                    }
                 }
+
+                // собрали все новые сообщения, сортируем в порядке возрастания даты (новые снизу)
+                chatMessages.sort( Comparator.comparing( ChatMessage::getMoment ) ) ;
+
+                // отображаем
                 runOnUiThread( this::showChatMessages ) ;
             }
             else {
@@ -155,8 +219,57 @@ public class ChatActivity extends AppCompatActivity {
             Log.d( "parseContent", ex.getMessage() ) ;
         }
     }
+    private void showChatMessages() {
+        Drawable otherBg = AppCompatResources.getDrawable(
+                getApplicationContext(), R.drawable.rates_bg ) ;
+        Drawable myBg = AppCompatResources.getDrawable(
+                getApplicationContext(), R.drawable.rates_bg_odd ) ;
+
+        LinearLayout.LayoutParams otherParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT ) ;
+        otherParams.setMargins( 7, 5, 7, 5 ) ;
+        LinearLayout.LayoutParams myParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT ) ;
+        myParams.setMargins( 7, 5, 7, 5 ) ;
+        myParams.gravity = Gravity.END ;
+
+        boolean needScrollDown = false ;
+        String author = etAuthor.getText().toString() ;
+        for( ChatMessage chatMessage : this.chatMessages ) {
+            if( chatMessage.getView() != null ) continue ;  // сообщение отображается - не новое
+
+            TextView tv = new TextView(this);
+            String msg = chatMessage.getMoment() + " " + chatMessage.getAuthor() + ": \n"
+                    + chatMessage.getTxt();
+            tv.setText(msg);
+            if (chatMessage.getAuthor().equals(author)) {
+                tv.setBackground(myBg);
+                tv.setLayoutParams(myParams);
+            } else {
+                tv.setBackground(otherBg);
+                tv.setLayoutParams(otherParams);
+            }
+            tv.setPadding(7, 5, 7, 5);
+            chatContainer.addView(tv);
+            chatMessage.setView(tv);
+            needScrollDown = true ;
+        }
+        if( needScrollDown ) {
+            svContainer.post( () -> svContainer.fullScroll( View.FOCUS_DOWN ) ) ;
+        }
+    }
 
     private static class ChatMessage {   // ORM for JSON data
+        private View view ;   // reference to TextView
+        public View getView() {
+            return view;
+        }
+        public void setView(View view) {
+            this.view = view;
+        }
+
         private UUID id ;
         private String author ;
         private String txt ;
